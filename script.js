5624a7d80400587cb6d13074d68a3ec791312543
dan <dontsendmail1@gmail.com>
2024-11-19 01:09:18 +0200

    upgrade

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 window.onload = function () {
   var introText = document.getElementById("intro-text");
 
+  var welcomeText = document.getElementById("welcome-text");
+  welcomeText.classList.add("activate-letter-spacing-effect");
   // Function to show and hide the text with a delay
   function toggleText() {
     // Show the text
-    introText.classList.add("visible");
+    introText.classList.add("toggle-visibility");
 
     // After 5 seconds, hide the text again
-    setTimeout(function () {
-      introText.classList.remove("visible"); // Hide the text
-    }, 5000); // 5 seconds delay before disappearing
   }
 
   // Trigger the function immediately when the page loads
   toggleText();
 
   // Repeat the cycle every 10 seconds (5 seconds to appear + 5 seconds to disappear)
-  setInterval(toggleText, 10000);
+  setInterval(toggleText, 500);
 };
